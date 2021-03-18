0519d7c7e74ff77d0ef7633298d5672ac868b01f
vino <vino0908@outlook.com>
2021-03-18 23:04:48 +0800

    完成翻译功能

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import * as https from 'https';
 import * as querystring from 'querystring';
 import md5 = require('md5');
+import {appid, secretKey} from './private';
 
 export const translate = (word) => {
 
-    const appid = '???';
-    const secretKey = '???';
     const salt = Math.random();
     const sign = md5(appid + word + salt + secretKey);
     const query: string = querystring.stringify({
@@ -22,15 +21,38 @@ export const translate = (word) => {
         path: `/api/trans/vip/translate?${query}`,
         method: 'GET'
     };
-    const req = https.request(options, (res) => {
+    const request = https.request(options, (response) => {
+        const chunks = [];
+        response.on('data', (chunk) => {
+            chunks.push(chunk);
+        });
+        response.on('end', () => {
+            const string = Buffer.concat(chunks).toString();
+            type HeiduResult = {
+                error_code?: string,
+                error_msg?: string,
+                from: string,
+                to: string,
+                trans_result: {
+                    src: string,
+                    dst: string
+                }[]
 
-        res.on('data', (d) => {
-            const result = process.stdout.write(d);
+            }
+            const object: HeiduResult = JSON.parse(string);
+            if (object.error_code) {
+                console.error(`ErrorMessage:${object.error_msg}`);
+                process.exit(2);
+            } else {
+                console.log(object.trans_result[0].dst);
+                process.exit(0);
+            }
+
         });
     });
 
-    req.on('error', (e) => {
+    request.on('error', (e) => {
         console.error(e);
     });
-    req.end();
+    request.end();
 };
---
+++ src/private.ts
@@ -0,0 +1,2 @@
+export const appid = '20201027000599859';
+export const secretKey = 'TML4Nwx4UXlb8sOy2J27';
