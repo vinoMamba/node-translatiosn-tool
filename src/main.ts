438c3d60972f26e4c140e8048e8e59faa1f62bf3
vino <vino0908@outlook.com>
2021-03-18 22:34:17 +0800

    引入heidufanyi-api

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,36 @@
+import * as https from 'https';
+import * as querystring from 'querystring';
+import md5 = require('md5');
+
 export const translate = (word) => {
-    console.log(word);
+
+    const appid = '???';
+    const secretKey = '???';
+    const salt = Math.random();
+    const sign = md5(appid + word + salt + secretKey);
+    const query: string = querystring.stringify({
+        q: word,
+        from: 'en',
+        to: 'zh',
+        appid,
+        salt,
+        sign
+    });
+    const options = {
+        hostname: 'api.fanyi.baidu.com',
+        port: 443,
+        path: `/api/trans/vip/translate?${query}`,
+        method: 'GET'
+    };
+    const req = https.request(options, (res) => {
+
+        res.on('data', (d) => {
+            const result = process.stdout.write(d);
+        });
+    });
+
+    req.on('error', (e) => {
+        console.error(e);
+    });
+    req.end();
 };
